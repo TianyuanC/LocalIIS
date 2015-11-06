3ac69319ceb8a0c44f711a336366dd15ebee1ae6
Ryan Chu <tianyuan.chu@gmail.com>
2015-11-06 10:32:39 -0800

    check admin

--- IISSwitcher/Program.cs
+++ IISSwitcher/Program.cs
@@ -6,8 +6,12 @@ namespace IISSwitcher
     {
         public static void Main(string[] args)
         {
+            var user = new User();
+            user.Check();
+            
             var local = new LocalIIS();           
             local.RunTask();
+            
             Console.ReadLine();
         }
     }
---
+++ IISSwitcher/User.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Principal;
+using System;
+
+namespace IISSwitcher
+{
+    /// <summary>
+    /// Class User.
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// The current
+        /// </summary>
+        private WindowsIdentity Current;
+
+        /// <summary>
+        /// Gets a value indicating whether this instance is admin.
+        /// </summary>
+        /// <value><c>true</c> if this instance is admin; otherwise, <c>false</c>.</value>
+        public bool IsAdmin
+        {
+            get
+            {
+                WindowsPrincipal principal = new WindowsPrincipal(Current);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="User"/> class.
+        /// </summary>
+        public User()
+        {
+            Current = WindowsIdentity.GetCurrent();
+        }
+
+        public void Check()
+        {
+            if (IsAdmin)
+            {
+                return;
+            }
+            Console.WriteLine("Needs admin access. Exiting...");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+    }
+}
